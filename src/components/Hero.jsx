9e0f6379fe564e191b4ad59595ca81bab6cce42f
benhendriks <benhendriks@protonmail.com>
2023-03-12 20:42:51 +0100

    Added canvas and first 3d scene

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0  top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className='w-5 h-5 rounded-full bg-gold'/>
           <div className="w-1 sm:h-80 h-40 gold-gradient"></div>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-silver`}>Hi I`m Ben, a <span className='text-gold'>Webdeveloper</span> baed in Basel.</h1>
-          <p className={`${styles.heroSubText} mt-2 text-silver`}>I develop modern 3d animated<br className='sm:block hidden' />  web applications</p>
+          <h1 className={`${styles.heroHeadText} text-silver`}>Hi I`m Ben, a<span className='text-gold'>Webdeveloper</span> baed in Basel.</h1>
+          <p className={`${styles.heroSubText} mt-2 text-silver`}>I develop modern 3d animated<br className='sm:block hidden' /> web applications</p>
         </div>
       </div>
-      <ComputerCanvas />
+      <ComputersCanvas />
     </section>
   )
 }
