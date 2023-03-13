44317b7fe2fa4f29bce8bd995c36803b8992b225
benhendriks <benhendriks@protonmail.com>
2023-03-13 22:15:56 +0100

    Added animated motion btn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputersCanvas } from "./canvas";
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-gold'/>
-          <div className="w-1 sm:h-80 h-40 gold-gradient"></div>
+          <div className='w-1 sm:h-80 h-40 gold-gradient'></div>
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-silver`}>Hi I`m Ben, a<span className='text-gold'>Webdeveloper</span> baed in Basel.</h1>
           <p className={`${styles.heroSubText} mt-2 text-silver`}>I develop modern 3d animated<br className='sm:block hidden' /> web applications</p>
-        </div>
+        </div> 
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='/about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center itmes-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
