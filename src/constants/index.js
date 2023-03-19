9a8c90e50cb231f7784a6138812da86711c90305
benhendriks <benhendriks@protonmail.com>
2023-03-19 10:26:35 +0100

    Added work page

--- src/constants/index.js
+++ src/constants/index.js
@@ -245,10 +245,6 @@ const projects = [
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
       {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
         name: "supabase",
         color: "green-text-gradient",
       },
